13d1ff92f40478b4a3da84590ce4f8f23d22008c
Ryan Pethel <arpethel@gmail.com>
2019-04-27 13:31:30 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -91,7 +91,7 @@ class MusicLibraryController
     binding.pry
     while input.between?(1, Song.all.length)
       # binding.pry
-      song = list_of_songs[input]
+      song = list_of_songs[input - 1]
       puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
