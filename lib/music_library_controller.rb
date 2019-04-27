7d0bc4565226257e606b455ead985c46cb24795e
Ryan Pethel <arpethel@gmail.com>
2019-04-27 13:34:26 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -43,19 +43,16 @@ class MusicLibraryController
   def list_songs
     songs_sorted_by_name = Song.all.sort_by {|song| song.name}
     songs_sorted_by_name.each.with_index(1) {|song, index| puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
-    # binding.pry
   end
 
   def list_artists
     songs_sorted_by_artist = Artist.all.sort_by {|artist| artist.name}
     songs_sorted_by_artist.each.with_index(1) {|artist, index| puts "#{index}. #{artist.name}"}
-    # binding.pry
   end
 
   def list_genres
     songs_sorted_by_genre = Genre.all.sort_by {|genre| genre.name}
     songs_sorted_by_genre.each.with_index(1) {|genre, index| puts "#{index}. #{genre.name}"}
-    # binding.pry
   end
 
 
@@ -68,7 +65,6 @@ class MusicLibraryController
       songs_sorted_by_name = artist.songs.sort_by {|song| song.name}
       songs_sorted_by_name.each.with_index(1) {|song, index| puts "#{index}. #{song.name} - #{song.genre.name}"}
     end
-    # binding.pry
   end
 
   def list_songs_by_genre
@@ -80,7 +76,6 @@ class MusicLibraryController
       songs_sorted_by_name = genre.songs.sort_by {|song| song.name}
       songs_sorted_by_name.each.with_index(1) {|song, index| puts "#{index}. #{song.artist.name} - #{song.name}"}
     end
-    # binding.pry
   end
 
   def play_song
@@ -88,13 +83,10 @@ class MusicLibraryController
     list_of_songs =  Song.all.sort {|a, b| a.name <=> b.name}
 
     input = gets.strip.to_i
-    # binding.pry
+
     if input.between?(1, Song.all.length)
-      # binding.pry
       song = list_of_songs[input - 1]
       puts "Playing #{song.name} by #{song.artist.name}"
-
     end
-    # binding.pry
   end
 end
