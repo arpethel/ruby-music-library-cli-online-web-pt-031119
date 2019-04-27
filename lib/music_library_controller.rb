339de071dd548b1b90d3f28cb1af7713c6b948af
Ryan Pethel <arpethel@gmail.com>
2019-04-27 13:15:36 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -81,7 +81,7 @@ class MusicLibraryController
 
   def play_song
     # puts "Choose a song from the list."
-    input = gets.strip.downcase
+    # input = gets.strip.downcase
     # binding.pry
     puts "Which song number would you like to play?"
     list_of_songs =  Song.all.sort {|a, b| a.name <=> b.name}
@@ -89,11 +89,16 @@ class MusicLibraryController
     input = gets.strip.to_i
     binding.pry
 
-    if (1..Song.all.length).include?(input)
-      binding.pry
+    # if (1..Song.all.length).include?(input)
+    #   binding.pry
+    #   song = list_of_songs[input + 2]
+    #   puts "Playing #{song.name} by #{song.artist.name}"
+    # 
+    # end
+
+    if input.between?(1..Song.all.length)
       song = list_of_songs[input + 2]
       puts "Playing #{song.name} by #{song.artist.name}"
-
     end
   end
 end
