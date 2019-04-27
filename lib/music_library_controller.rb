29172bf3b8757a9d38d9994eecd09d69df33226f
Ryan Pethel <arpethel@gmail.com>
2019-04-27 12:10:01 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -84,7 +84,7 @@ class MusicLibraryController
     # input = gets.strip
     list_of_songs =  Song.all.sort {|a, b| a.name <=> b.name}
     input = gets.strip.to_i
-    binding.pry
+    # binding.pry
     if (1..Song.all.length).include?(input)
       binding.pry
       song = list_of_songs[input+2]
