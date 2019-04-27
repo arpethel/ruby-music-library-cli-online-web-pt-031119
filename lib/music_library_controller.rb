58c69a4765b464393268ae23de73b8e1e1455fbe
Ryan Pethel <arpethel@gmail.com>
2019-04-27 13:17:29 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -96,7 +96,7 @@ class MusicLibraryController
     #
     # end
 
-    if input.between?(1..Song.all.length)
+    if input.between?(1, Song.all.length)
       song = list_of_songs[input + 2]
       puts "Playing #{song.name} by #{song.artist.name}"
     end
